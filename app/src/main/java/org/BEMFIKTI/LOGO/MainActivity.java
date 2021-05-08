90d6cc62367198296a1359710fffb04c60a7abd1
Faiz Authar <faizauthar@gmail.com>
2021-05-08 12:00:39 +0700

    logic MainActivity: menghubungkan layout dengan logic pemrograman

--- app/src/main/java/org/BEMFIKTI/LOGO/MainActivity.java
+++ app/src/main/java/org/BEMFIKTI/LOGO/MainActivity.java
@@ -3,12 +3,26 @@ package org.BEMFIKTI.LOGO;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Membuat variable widget.
+    private Button btnAdd;
+    private Button btnSubstract;
+    private Button btnClear;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // menghubungkan variable kepada widget yang berada di layout.
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSubstract = findViewById(R.id.btnSubstract);
+        btnClear = findViewById(R.id.btnClear);
+        tvResult = findViewById(R.id.tvResult);
     }
 }
