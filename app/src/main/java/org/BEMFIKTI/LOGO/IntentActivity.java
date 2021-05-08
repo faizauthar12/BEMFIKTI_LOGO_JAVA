f73f04740e26ebecf782d051f47a9ff6afe3366f
Faiz Authar <faizauthar@gmail.com>
2021-05-08 14:25:10 +0700

    logic acitivty ke2: tambahkan logika back button

--- app/src/main/java/org/BEMFIKTI/LOGO/IntentActivity.java
+++ app/src/main/java/org/BEMFIKTI/LOGO/IntentActivity.java
@@ -1,8 +1,11 @@
 package org.BEMFIKTI.LOGO;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class IntentActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class IntentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
+
+        // panggil actionbar
+        ActionBar actionBar = getSupportActionBar();
+
+        // munculin tombol back
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    // buat logika ketika tombol back pada action bar ditekan.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
